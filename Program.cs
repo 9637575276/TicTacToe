1cdb5c97768f30fd5099a8eac125d021f5e97622
Shubham Mule <shubhambmule@gmail.com>
2021-10-17 13:12:05 +0530

    Checking players choice

--- Program.cs
+++ Program.cs
@@ -4,21 +4,46 @@ namespace TicTacToeGame
 {
     class Program
     {
-        //making array and   where no use of zero 
+       
 
         static char[] arr = { '0', '1', '2', '3', '4', '5', '6', '7', '8', '9' };
 
-        static int player = 1; //By default player 1 is set  
+        static int player = 1;
 
-        static int choice; //This holds the choice at which position user want to mark   
+        static int choice;  
 
 
         static void Main(string[] args)
         {
+
+
+            Console.WriteLine("Player1:X and Player2:O");
+
+            Console.WriteLine("\n");
+
+            if (player % 2 == 0)//checking the chance of the player  
+
+            {
+
+                Console.WriteLine("Player 2 Chance");
+
+            }
+
+            else
+
+            {
+
+                Console.WriteLine("Player 1 Chance");
+
+            }
+
+            Console.WriteLine("\n");
             Board();
+
+
         }
 
-        // create board method
+        
         private static void Board()
 
         {
